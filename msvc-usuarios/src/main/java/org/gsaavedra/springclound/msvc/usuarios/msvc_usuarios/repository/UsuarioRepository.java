29fa1c019ee20fb7291e6ec31395aa8ec98c9332
GermanSaavedra <saavedraorozcog@gmail.com>
2025-02-04 11:32:27 -0500

    feat: microservicio usuarios

---
+++ msvc-usuarios/src/main/java/org/gsaavedra/springclound/msvc/usuarios/msvc_usuarios/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package org.gsaavedra.springclound.msvc.usuarios.msvc_usuarios.repository;
+
+import org.gsaavedra.springclound.msvc.usuarios.msvc_usuarios.models.entity.Usuario;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+
+}
