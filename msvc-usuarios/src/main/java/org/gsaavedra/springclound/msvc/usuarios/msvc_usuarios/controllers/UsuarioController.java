7f25f177f3a66735de87d019fb75e71559e55d4e
GermanSaavedra <saavedraorozcog@gmail.com>
2025-02-05 11:25:13 -0500

    feat: Impl. RestController usuario

---
+++ msvc-usuarios/src/main/java/org/gsaavedra/springclound/msvc/usuarios/msvc_usuarios/controllers/UsuarioController.java
@@ -0,0 +1,62 @@
+package org.gsaavedra.springclound.msvc.usuarios.msvc_usuarios.controllers;
+
+import org.gsaavedra.springclound.msvc.usuarios.msvc_usuarios.models.entity.Usuario;
+import org.gsaavedra.springclound.msvc.usuarios.msvc_usuarios.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService service;
+
+    @GetMapping
+    public List<Usuario> listar() {
+        return service.listar();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> detalle(@PathVariable Long id){
+        Optional<Usuario> usuarioOptional = service.porId(id);
+        if(usuarioOptional.isPresent()){
+            return ResponseEntity.ok(usuarioOptional.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<?> crear(@RequestBody Usuario usuario){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(usuario));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editar(@RequestBody Usuario usuario, @PathVariable Long id) {
+        Optional<Usuario> o = service.porId(id);
+        if(o.isPresent()){
+            Usuario usuarioDb = o.get();
+            usuarioDb.setNombre(usuario.getNombre());
+            usuarioDb.setEmail(usuario.getEmail());
+            usuarioDb.setPassword(usuario.getPassword());
+            return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(usuarioDb));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id){
+        Optional<Usuario> o = service.porId(id);
+        if(o.isPresent()){
+            service.eliminar(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
